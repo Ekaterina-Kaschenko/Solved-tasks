d84548963a482eb896ccc2a6c9cd6396f4e9853b
Ekaterina Kashchenko <katikate123@gmail.com>
2016-09-02 17:39:41 +0300

    fixed 2 task

--- Lesson 1-2/2/scripts/script.js
+++ Lesson 1-2/2/scripts/script.js
@@ -1,32 +1,37 @@
 function setName() {
 	var names = [];
-  var currentName, access;
+	var currentName;
 	var i = 0;
-	while (i < 3) {
-    currentName = prompt('Enter name', '');
-    if (currentName != '') {
-		  names.push(currentName);
-      i++; 
-	  } else {
-      alert ('Try to enter your name again');
-    }
-  }
-  
-  var enterName = prompt('Enter your name', enterName);
-  
-	names.forEach(function(item, i) {
-		if (names.indexOf(enterName) < 0) {
-			return access = false;
+	while (i < 2) { 
+		currentName = prompt('Enter name', ''); 
+		if (currentName !== ''){
+			names.push(currentName); 
+			i++; 
 		} else {
-      return access = true;
+			alert('you should enter name');
 		}
-	})
-  
-  return access ? enterName + ', вы успешно вошли!' : 'Ты кто?';
-  
+	}
+	return names;
 }
 
-alert(setName());
+function checkName() {
+	var names = setName();
+  
+  while(true) {
+  	var currentName = prompt('Enter your name', currentName);
+  
+    if (currentName === '') {
+      alert('you should enter name');
+    } else if (names.indexOf(currentName) >= 0) {
+      alert(currentName + ', вы успешно вошли');
+      break;
+    } else {
+      alert('Ты кто?');
+      break;
+    }
+  }
+	
+	return names;
+}
 
-
-
+console.log(checkName());
