8b0231b2c66cadeb5d91a82eeea874f385725605
Ekaterina Kashchenko <katikate123@gmail.com>
2016-09-06 12:18:11 +0300

    tabs done

--- Lesson 7-8/1/scripts/script.js
+++ Lesson 7-8/1/scripts/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-  $(".tabs__item:first").addClass("current");
+  $(".tabs__item:first").children().addClass("current");
 
   $('.tabs__item').click(function (e) {
     e.preventDefault();
     var _href = $(this).children().attr("href");
     console.log(_href);
-    $(".tabs__item").removeClass("current");
+    $(".tabs__item").children().removeClass("current");
     $(this).children().addClass("current");
     $(".tabs-content > div").hide();
     $(_href).fadeIn();
