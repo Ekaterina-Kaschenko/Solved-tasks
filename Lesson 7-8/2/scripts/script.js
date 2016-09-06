c41f29e90cda2491962278c20ac0c1754bb5fea2
Ekaterina Kashchenko <katikate123@gmail.com>
2016-09-06 22:50:18 +0300

    form with tips done

--- Lesson 7-8/2/scripts/script.js
+++ Lesson 7-8/2/scripts/script.js
@@ -1,13 +1,30 @@
 $(document).ready(function(){
-  $(".tabs__item:first").addClass("current");
+  function createLayout(input, tip) {
+    var $input = $(input),
+        container = $input.closest('div');
+    if (container.hasClass('form__item') && container.find('div.tip').length > 0) {
+      return;
+    }
 
-  $('.tabs__item').click(function (e) {
-    e.preventDefault();
-    var _href = $(this).children().attr("href");
-    console.log(_href);
-    $(".tabs__item").removeClass("current");
-    $(this).children().addClass("current");
-    $(".tabs-content > div").hide();
-    $(_href).fadeIn();
+    container.addClass('form__item');
+    var elem = document.createElement('div');
+    $(elem).addClass('tip').html(tip); 
+    container.append(elem);
+    return elem;
+  }
+
+  $('input').on('mouseover', function () {
+    var tip = $(this).prop('title');
+    createLayout(this, tip);
+  });
+  
+  $('input').on('mouseleave', function () {
+  var cont = $(this).closest('div.form__item');
+  console.log('mouse leave', cont);
+    if (cont.count < 0) {
+      return;
+    }
+
+    cont.find('div.tip').remove();
   });
 })
