6c89164394eb76a6127b9563363fa351ba6d133e
Ekaterina Kashchenko <katikate123@gmail.com>
2016-09-09 01:30:17 +0300

    fixed timer

--- Lesson 5-6/1/scripts/script.js
+++ Lesson 5-6/1/scripts/script.js
@@ -1,44 +1,34 @@
 'use strict';
 
-var elements = {
-  start: document.getElementsByClassName('start')[0],
-  continue: document.getElementsByClassName('continue')[0],
-  pause: document.getElementsByClassName('pause')[0],
-  clear: document.getElementsByClassName('clear')[0],
-  milliseconds: document.getElementsByClassName('ms')[0],
-  seconds: document.getElementsByClassName('sec')[0],
-  minutes: document.getElementsByClassName('min')[0],
-  hours: document.getElementsByClassName('hours')[0],
-  intervar: '100'
-};
-
 function Timer () {
-  elements.start.addEventListener('click', function () {
+  this.element = {}
+  this.getElements();
+  this.element.start.addEventListener('click', function () {
     var currentTime = Date.now();
-    elements.start.style.display = 'none';
-    elements.pause.style.display = 'inline-block';
+    this.element.start.style.display = 'none';
+    this.element.pause.style.display = 'inline-block';
     this.start();
   }.bind(this));
     
-  elements.continue.addEventListener('click', function () {
+  this.element.continue.addEventListener('click', function () {
     console.log('continue');
-    elements.continue.style.display = 'none';
-    elements.pause.style.display = 'inline-block';
+    this.element.continue.style.display = 'none';
+    this.element.pause.style.display = 'inline-block';
     this.continue();
   }.bind(this));
 
-  elements.pause.addEventListener('click', function () {
+  this.element.pause.addEventListener('click', function () {
     console.log('pause');
-    elements.pause.style.display = 'none';
-    elements.continue.style.display = 'inline-block';
+    this.element.pause.style.display = 'none';
+    this.element.continue.style.display = 'inline-block';
     this.pause();
   }.bind(this));
 
-  elements.clear.addEventListener('click', function () {
+  this.element.clear.addEventListener('click', function () {
     console.log('clear');
-    elements.continue.style.display = 'none';
-    elements.pause.style.display = 'none';
-    elements.start.style.display = 'inline-block';
+    this.element.continue.style.display = 'none';
+    this.element.pause.style.display = 'none';
+    this.element.start.style.display = 'inline-block';
     this.clear();
   }.bind(this));
 }
@@ -49,39 +39,51 @@ Timer.prototype = {
      this.updateTime();
   },
   pause: function () {
-  	clearInterval(this.interval);
-  	this.pauseTime = Date.now();
+    clearInterval(this.interval);
+    this.pauseTime = Date.now();
   },
   continue: function () {
-  	this.currentTime = new Date(this.currentTime - (this.pauseTime - Date.now()));
-  	this.updateTime();
-  	
+    this.currentTime = new Date(this.currentTime - (this.pauseTime - Date.now()));
+    this.updateTime();
+    
   },
   clear: function () {
-  	this.pause();
-  	this.updateText();
+    this.pause();
+    this.updateText();
   },
   updateTime: function () {
-  	this.interval = setInterval(function() {
+    this.interval = setInterval(function() {
       var diferenceDate = new Date(Date.now() - this.currentTime);
-      elements.milliseconds.innerHTML = this.beatifyViewText(diferenceDate.getUTCMilliseconds());
-      elements.seconds.innerHTML = this.beatifyViewText(diferenceDate.getUTCSeconds());
-      elements.minutes.innerHTML = this.beatifyViewText(diferenceDate.getUTCMinutes());
-      elements.hours.innerHTML = this.beatifyViewText(diferenceDate.getUTCHours());
-  	}.bind(this));
+      this.element.milliseconds.innerHTML = this.beatifyViewText(diferenceDate.getUTCMilliseconds(), 3);
+      this.element.seconds.innerHTML = this.beatifyViewText(diferenceDate.getUTCSeconds());
+      this.element.minutes.innerHTML = this.beatifyViewText(diferenceDate.getUTCMinutes());
+      this.element.hours.innerHTML = this.beatifyViewText(diferenceDate.getUTCHours());
+    }.bind(this));
   },
   updateText: function (argument) {
-  	elements.milliseconds.innerHTML = '00';
-  	elements.seconds.innerHTML = '00';
-  	elements.minutes.innerHTML = '00';
-  	elements.hours.innerHTML = '00';
+    this.element.milliseconds.innerHTML = '00';
+    this.element.seconds.innerHTML = '00';
+    this.element.minutes.innerHTML = '00';
+    this.element.hours.innerHTML = '00';
   },
   beatifyViewText: function (number, count) {
-  	count = count || 2;
+    count = count || 2;
     return ('00' + number).slice(-count);
+  },
+  getElements: function () {
+    this.element.start = document.getElementsByClassName('start')[0],
+    this.element.continue = document.getElementsByClassName('continue')[0],
+    this.element.pause = document.getElementsByClassName('pause')[0],
+    this.element.clear = document.getElementsByClassName('clear')[0],
+    this.element.milliseconds = document.getElementsByClassName('ms')[0],
+    this.element.seconds = document.getElementsByClassName('sec')[0],
+    this.element.minutes = document.getElementsByClassName('min')[0],
+    this.element.hours = document.getElementsByClassName('hours')[0],
+    this.element.intervar = '100'
   }
 
 };
 
 
 var timer = new Timer ();
+console.log(timer)
