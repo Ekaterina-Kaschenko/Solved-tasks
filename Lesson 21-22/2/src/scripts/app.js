8d87f233242160914267a95ebf5779dfcd2bb6ec
Ekaterina Kashchenko <katikate123@gmail.com>
2016-09-28 12:41:06 +0300

    es6 task done

--- Lesson 21-22/2/src/scripts/app.js
+++ Lesson 21-22/2/src/scripts/app.js
@@ -1,81 +1,83 @@
 import questionsData from './script.js';
 import _ from 'lodash';
 
-function Test() {
-  this.init();
-}
+class Test{
+  constructor() {
+    this.init();
+  }
 
-Test.prototype.getTemplate = () => {
-  let tmplTest = document.getElementById('grid-template').innerHTML;
-  tmplTest = _.template(tmplTest);
+  init() {
+    this.getTemplate();
+    this.checkCorrectAnswer();
+  };
 
-  document.getElementsByClassName('section')[0].innerHTML += tmplTest({
-    list: questionsData
-  });
 
-  let tmplButton = document.getElementById('button').innerHTML;
-  tmplButton = _.template(tmplButton);
+  getTemplate() {
+    let tmplTest = document.getElementById('grid-template').innerHTML;
+    tmplTest = _.template(tmplTest);
 
-  document.getElementsByClassName('section')[0].innerHTML += tmplButton({
+    document.getElementsByClassName('section')[0].innerHTML += tmplTest({
+      list: questionsData
+    });
 
-  });
+    let tmplButton = document.getElementById('button').innerHTML;
+    tmplButton = _.template(tmplButton);
 
-  let tmplModal = document.getElementById('modal').innerHTML;
-  tmplModal = _.template(tmplModal);
+    document.getElementsByClassName('section')[0].innerHTML += tmplButton({
 
-  document.getElementsByClassName('section')[0].innerHTML += tmplModal({
+    });
 
-  });
-};
+    let tmplModal = document.getElementById('modal').innerHTML;
+    tmplModal = _.template(tmplModal);
 
-Test.prototype.localStorageInit = obj => {
-  localStorage.setItem('testObject', JSON.stringify(obj));
-  let textObj = localStorage.getItem('testObject');
-  return JSON.parse(textObj);
-};
+    document.getElementsByClassName('section')[0].innerHTML += tmplModal({
 
-Test.prototype.checkCorrectAnswer = function () {
-  let self = this;
-  let allAnswers = questionsData.length;
-  let answer = this.localStorageInit(questionsData);
-  let inputs = Array.prototype.slice.call(document.getElementsByClassName('answer'));
-  let checkButton =  document.getElementsByClassName('check-button')[0];
-  checkButton.addEventListener('click', function () {
-    self.countAnswer = 0;
-    self.correctAnswers = new Array(questionsData.length);
-    inputs.forEach(function (el) {
-      let dataQuestion = el.getAttribute('data-question');
-      let dataId = el.getAttribute('data-id');
-      let answer = questionsData[dataQuestion].answers[dataId];
+    });
+  };
 
-      if (answer.isRight && el.checked && self.correctAnswers[dataQuestion] !== false) {
-        self.correctAnswers[dataQuestion] = true;
-      } else if (answer.isRight && !el.checked || !answer.isRight && el.checked) {
-        self.correctAnswers[dataQuestion] = false;
+  localStorageInit(obj) {
+    localStorage.setItem('testObject', JSON.stringify(obj));
+    let textObj = localStorage.getItem('testObject');
+    return JSON.parse(textObj);
+  };
+
+  checkCorrectAnswer() {
+    let self = this;
+    let allAnswers = questionsData.length;
+    let answer = this.localStorageInit(questionsData);
+    let inputs = Array.prototype.slice.call(document.getElementsByClassName('answer'));
+    let checkButton =  document.getElementsByClassName('check-button')[0];
+    checkButton.addEventListener('click', function () {
+      self.countAnswer = 0;
+      self.correctAnswers = new Array(questionsData.length);
+      inputs.forEach(function (el) {
+        let dataQuestion = el.getAttribute('data-question');
+        let dataId = el.getAttribute('data-id');
+        let answer = questionsData[dataQuestion].answers[dataId];
+
+        if (answer.isRight && el.checked && self.correctAnswers[dataQuestion] !== false) {
+          self.correctAnswers[dataQuestion] = true;
+        } else if (answer.isRight && !el.checked || !answer.isRight && el.checked) {
+          self.correctAnswers[dataQuestion] = false;
+        }
+      });
+
+      for (let i = 0; i < self.correctAnswers.length; i++) {
+        if (!self.correctAnswers[i]) {
+          self.countAnswer = self.countAnswer;
+        } else {
+          self.countAnswer++;
+        }
       }
     });
 
-    for (let i = 0; i < self.correctAnswers.length; i++) {
-      if (!self.correctAnswers[i]) {
-        self.countAnswer = self.countAnswer;
-      } else {
-        self.countAnswer++;
-      }
+  };
+
+  clearCheckboxes() {
+    let checkboxes = document.getElementsByClassName('answer');
+    for (let i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].checked = false;
     }
-  });
-
-};
-
-Test.prototype.clearCheckboxes = () => {
-  let checkboxes = document.getElementsByClassName('answer');
-  for (let i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].checked = false;
   }
 }
-
-Test.prototype.init = function () {
-  this.getTemplate();
-  this.checkCorrectAnswer();
-};
-
 export default Test;
